702fb9bbe4c6646cb559ac79c9a7a547f9a01dc1
aogao <dhugaoao@163.com>
2022-09-25 02:09:40 +0800

    首次测试PyCharm自带git使用

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def print_hi_maskFCN():
+    # test for upload
+    print("hello maskRCN")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    print_hi_maskFCN()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
